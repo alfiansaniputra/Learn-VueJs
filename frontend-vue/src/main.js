77b2bace18e094c4bad9c8d718b5efc25c359f6e
fianmuhamad <alfiansaniputra77@gmail.com>
2022-11-23 10:41:25 +0700

    add axios for HTTP Request and add Bootstrap 5.1 for devlop frontend

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -1,4 +1,16 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min'
+
+const app = createApp(App)
+
+app.mount('#app')
