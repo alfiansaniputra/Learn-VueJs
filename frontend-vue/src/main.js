7617ae0a955dea87ab5c9ebbb1d249558388eef1
fianmuhamad <alfiansaniputra77@gmail.com>
2022-11-23 11:00:51 +0700

    import router to main.js, and use router package

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -11,6 +11,12 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min'
 
+//import router
+import router from './router'
+
 const app = createApp(App)
+
+//use vue router
+app.use(router)
 
 app.mount('#app')
